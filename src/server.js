fb1336617ac1e8ec966d5adc8430cd1b9ce425d6
GABER77 <mg7965724@gmail.com>
2025-05-05 21:18:51 +0300

    Handling uncaught Exception

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ import app from './app.js';
 
 dotenv.config({ path: '../config.env' });
 
+// Errors in sync code
+process.on('uncaughtException', (err) => {
+  console.log('❌ Uncaught Exception, Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
