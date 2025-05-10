ab4b374dcd7050d046f15e78081e2f2863bcfcf0
GABER77 <mg7965724@gmail.com>
2025-05-10 15:25:14 +0300

    Add JWT token generation using user ID

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,24 @@
+import jwt from 'jsonwebtoken';
 import catchAsync from '../utils/catchAsync.js';
 import User from '../models/userModel.js';
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  user.password = undefined; // Don't include the hashed password in the response
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
 
 const signUp = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -8,10 +27,8 @@ const signUp = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-  res.status(201).json({
-    status: 'success',
-    data: { newUser },
-  });
+
+  createSendToken(newUser, 201, res);
 });
 
 const login = catchAsync(async (req, res, next) => {});
