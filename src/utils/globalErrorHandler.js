e2dc80a6be2a55854fb382817fd063975995180a
GABER77 <mg7965724@gmail.com>
2025-05-17 15:18:16 +0300

    Update global error handler with detailed support for DB and JWT errors

--- src/utils/globalErrorHandler.js
+++ src/utils/globalErrorHandler.js
@@ -1,6 +1,71 @@
+import CustomError from './customError.js';
+
+const handleCastErrorDB = (error) => {
+  const message = `Invalid ${error.path}: ${error.value}`;
+  return new CustomError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (error) => {
+  const matches = error.message?.match(/(["'])(\\?.)*?\1/);
+  const value = matches ? matches[0] : 'Field';
+  const message = `${value} already exists, please try a unique value`;
+  return new CustomError(message, 400);
+};
+
+const handleValidationErrorDB = (error) => {
+  const errors = Object.values(error.errors).map((el) => el.message);
+  const message = `Invalid input data: ${errors.join('; ')}`;
+  return new CustomError(message, 400);
+};
+
+const handleJWTError = () =>
+  new CustomError('Invalid token, Please login again', 401);
+
+const handleJWTExpiredError = () =>
+  new CustomError('Your session is expired, Please login again', 401);
+
+const sendErrorDev = (err, req, res) =>
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+
+const sendErrorProd = (err, req, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // Programming or 3rd party error: don't leak error details
+  console.error('❌ERROR:', err);
+  return res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong',
+  });
+};
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500; // 500 for internal server error
   err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = Object.create(err);
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error); // Invalid data like ID
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error); // unUnique value
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
+    sendErrorProd(error, req, res);
+  }
 };
 
 export default globalErrorHandler;
