1103f338f9df07a67ecb04101e6435a0b2873da6
GABER77 <mg7965724@gmail.com>
2025-05-25 20:38:27 +0300

    feat(utils): add reusable filterObject function to filter allowed fields

---
+++ src/utils/filterObject.js
@@ -0,0 +1,9 @@
+const filterObject = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) newObj[key] = obj[key];
+  });
+  return newObj;
+};
+
+export default filterObject;
